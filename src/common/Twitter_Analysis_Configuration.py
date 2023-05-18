68caf2c006a72a1177e1cd5d995da212de6c845c
tarvihafatima <tararqum@gmail.com>
2023-05-18 15:39:09 +0200

    Add files via upload

--- src/common/Twitter_Analysis_Configuration.py
+++ src/common/Twitter_Analysis_Configuration.py
@@ -2,22 +2,36 @@
 from typing import Optional
 from fastapi import FastAPI, Query
 
-
 # initialize global variables
-no_of_top_trends = 3
-country = 'netherlands'
+g_no_of_top_trends = 3
+g_country = 'netherlands'
+g_minutes = 10
 
 app = FastAPI()
  
 # Set Twitter Trends Configurations    
 @app.post("/trends_configurations/")
 def add_config(no_of_top_trends: int = Query(default= 3), country: str = Query(default= 'netherlands')):
-    no_of_top_trends = no_of_top_trends
-    country = country
-    return no_of_top_trends, country
+    global g_no_of_top_trends, g_country
+    g_no_of_top_trends = no_of_top_trends
+    g_country = country
+    return g_no_of_top_trends, g_country
 
 # Get Twitter Trends Configurations   
 @app.get("/trend_read_configurations/")
 def read_config():
-    return no_of_top_trends, country
+    global g_no_of_top_trends, g_country
+    return g_no_of_top_trends, g_country
 
+# Set Timer 
+@app.post("/timer/")
+def add_config(minutes: int = Query(default= 10)):
+    global g_minutes
+    g_minutes = minutes
+    return g_minutes
+
+# Get Twitter Trends Configurations   
+@app.get("/get_timer/")
+def read_config():
+    global g_minutes
+    return g_minutes
